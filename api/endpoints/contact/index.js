peter-vasarhelyi/suite-js-sdk

api.contact.fields(customerId)

api/endpoints/contact/index.js
@@ -11,6 +11,10 @@ Contact.prototype = {
   create: function(customerId, payload, options) {
     logger.log('contact_create');
     return this._request.post(customerId, '/contact', payload, options);
+  },
+
+  fields: function(customerId) {
+    return this._request.get(customerId, '/field');
   }
 
 };

ifroz/suite-js-sdk-legacy

api/endpoints/contact/index.js
@@ -17,13 +17,16 @@ _.extend(Contact.prototype, {
 
   create: function(payload, options) {
     logger.log('contact_create');
-
     return this._request.post(
       this._getCustomerId(options),
       '/contact',
       payload,
       options
     );
+  },
+
+  fields: function(options) {
+    return this._request.get(this._getCustomerId(options), '/field');
   }
 
 });